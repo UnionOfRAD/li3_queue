3fe61a11de0d53cd644efb4b513b3e3937723d1e
Christopher Garvis <cgarvis@gmail.com>
2012-02-28 13:13:03 -0500

    added tests for Queue

---
+++ tests/mocks/extensions/adapter/queue/MockQueue.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace li3_queue\tests\mocks\extensions\adapter\queue;
+
+class MockQueue extends \li3_queue\storage\Queue {
+	protected $_queue = array();
+
+	public function add($task, array $options = array()) {
+		$this->_queue[] = array('task' => $task, 'options' => $options);
+		return true;
+	}
+
+	public function run(array $options = array()) {
+		if (!empty($this->_queue)) {
+			array_shift($this->_queue);
+			return 'Success';
+		}
+
+		return false;
+	}
+
+	public function reset(array $options = array()) {
+		$this->_queue = array();
+	}
+}
+
+?>
