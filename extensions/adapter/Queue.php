ed40741c256fd142874c798d0092bbeccab1fe23
Mark Wilde <mwilde@ingenium-ids.org>
2014-04-03 17:02:36 +0100

    Fixed formatting

--- extensions/adapter/Queue.php
+++ extensions/adapter/Queue.php
@@ -3,11 +3,13 @@
 namespace li3_queue\extensions\adapter;
 
 abstract class Queue extends \lithium\core\Object {
+
 	abstract public function add($task, array $options = array());
 
 	abstract public function reset(array $options = array());
 
 	abstract public function run(array $options = array());
+
 }
 
 ?>
