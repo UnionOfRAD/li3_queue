e0cb95e3063239f76852e5ecdfb98baa9d348354
Mark Wilde <mwilde@ingenium-ids.org>
2014-04-09 16:50:31 +0100

    Updated mock queue to use new message object

--- tests/mocks/extensions/adapter/queue/MockQueue.php
+++ tests/mocks/extensions/adapter/queue/MockQueue.php
@@ -6,27 +6,64 @@ class MockQueue extends \li3_queue\extensions\adapter\Queue {
 
 	protected $_queue = array();
 
+	protected $_unacked = array();
+
+	public function connect() {
+		return true;
+	}
+
+	public function isConnected() {
+		return true;
+	}
+
+	public function disconnect() {
+		return true;
+	}
+
 	public function write($data, array $options = array()) {
-		$this->_queue[] = array('data' => $data, 'options' => $options);
+		$defaults = array('class' => 'message', 'priority' => 0);
+		$options += $defaults;
+		$queue = &$this->_queue;
+
+		$config = array(
+			'queue' => $this,
+			'data' => $data,
+			'priority' => $options['priority']
+		);
+		$message = $this->invokeMethod('_instance', array($options['class'], $config));
+		array_push($queue, $message);
 		return true;
 	}
 
 	public function read(array $options = array()) {
-		$queue = $this->_queue;
+		$queue = &$this->_queue;
 
 		if(!empty($queue)) {
-			$result = array_shift($queue);
-			return $result;
+			$message = array_shift($queue);
+			$this->_unacked[$message->id()] = $message;
+
+			return $message;
 		}
 		return null;
 	}
 
-	public function confirm() {
-		return null;
+	public function confirm($message) {
+		$unacked = &$this->_unacked;
+		unset($unacked[$message->id()]);
+
+		return true;
 	}
 
-	public function requeue() {
-		return null;
+	public function requeue($message) {
+		$queue = &$this->_queue;
+		$unacked = &$this->_unacked;
+		$message = $unacked[$message->id()];
+		unset($unacked[$message->id()]);
+
+		if(array_push($queue, $message)) {
+			return true;
+		}
+		return false;
 	}
 
 	public function consume($callback, array $options = array()) {
@@ -37,6 +74,28 @@ class MockQueue extends \li3_queue\extensions\adapter\Queue {
 			return $callback($result);
 		}
 		return null;
+	}
+
+	public function purge() {
+		$queue = &$this->_queue;
+		$unacked = &$this->_unacked;
+
+		$messages = count($queue);
+		$queue = array();
+		$unacked = array();
+
+		return $messages;
+	}
+
+	public function stats() {
+		$queue = &$this->_queue;
+		$unacked = &$this->_unacked;
+
+		$stats = array(
+			'queue' => count($queue),
+			'unacknowledged' => count($unacked)
+		);
+		return $stats;
 	}
 
 	public function add($task, array $options = array()) {
