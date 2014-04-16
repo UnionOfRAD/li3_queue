68fad6ec5fc7f9de2f4af8d44c15ab855af07eea
Mark Wilde <mwilde@ingenium-ids.org>
2014-04-16 14:15:10 +0100

    Set blocking on stream

---
+++ net/socket/Stream.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace li3_queue\net\socket;
+
+class Stream extends \lithium\net\socket\Stream {
+
+	public function open(array $options = array()) {
+		$resource = parent::open($options);
+		stream_set_blocking($resource, 0);
+		return $resource;
+	}
+
+}
+
+?>
