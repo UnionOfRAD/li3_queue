4c59f389654cbcd37ab8e6bf54bb33b7b95f17cf
Mark Wilde <mwilde@ingenium-ids.org>
2014-04-16 14:14:17 +0100

    Added Request/Response classes

---
+++ net/beanstalk/Request.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace li3_queue\net\beanstalk;
+
+class Request extends \lithium\core\Object {
+
+	/**
+	 * The method of the request, typically one of the following: `CHOOSE`, `PUT`, `RESERVE`.
+	 *
+	 * @var string
+	 */
+	protected $_method = null;
+
+	protected $_options = array();
+
+	protected $_data = null;
+
+	protected $_autoConfig = array(
+		'method',
+		'data',
+		'options'
+	);
+
+	/**
+	 * Array of closures that return various request strings for Beanstalk.
+	 *
+	 * @var array
+	 */
+
+	protected $_requestTypes = array();
+
+	public function __construct(array $config = array()) {
+		$defaults = array(
+			'method' => null,
+			'data' => null,
+			'options' => array()
+		);
+
+		$this->_requestTypes = array(
+			'use' => function($data, $options) {
+				return sprintf('use %s', $data);
+			},
+			'put' => function($data, $options) {
+				$defaults = array(
+					'pri' => 0,
+					'delay' => 0,
+					'ttl' => 0
+				);
+				$options += $defaults;
+				extract($options, EXTR_SKIP);
+
+				$bytes = strlen($data);
+
+				$command = sprintf('put %d %d %d %d', $pri, $delay, $ttl, $bytes);
+				return join("\r\n", array($command, $data));
+			},
+			'reserve' => function($data, $options) {
+				return 'reserve';
+			},
+			'reserve-with-timeout' => function($data, $options) {
+				$defaults = array(
+					'timeout' => 0
+				);
+				$options += $defaults;
+				extract($options, EXTR_SKIP);
+
+				return sprintf('reserve-with-timeout %d', $timeout);
+			},
+			'release' => function($id, $options) {
+				$defaults = array(
+					'pri' => 0,
+					'delay' => 0
+				);
+				$options += $defaults;
+				extract($options, EXTR_SKIP);
+
+				return sprintf('release %d %d %d', $id, $pri, $delay);
+			},
+			'delete' => function($id, $options) {
+				return sprintf('delete %d', $id);
+			},
+			'list-tubes' => function() {
+				return 'list-tubes';
+			},
+			'stats' => function() {
+				return 'stats';
+			}
+		);
+
+		return parent::__construct($config + $defaults);
+	}
+
+	public function body() {
+		$method = $this->_method;
+		$handlers = $this->_requestTypes;
+		$handler = isset($handlers[$method]) ? $handlers[$method] : null ;
+		return (string) $handler($this->_data, $this->_options);
+	}
+
+	/**
+	 * Magic method to convert object to string.
+	 * Finishes request with CR-LF pair
+	 *
+	 * @return string
+	 */
+	public function __toString() {
+		return (string) $this->body() . "\r\n";
+	}
+
+}
+
+?>
---
+++ net/beanstalk/Response.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace li3_queue\net\beanstalk;
+
+class Response extends \lithium\core\Object {
+
+	public $id = null;
+
+	public $status = null;
+
+	public $bytes = null;
+
+	public $data = null;
+
+	protected $_responseTypes = array(
+		'/(?<status>OUT_OF_MEMORY)/',
+		'/(?<status>INTERNAL_ERROR)/',
+		'/(?<status>BAD_FORMAT)/',
+		'/(?<status>UNKNOWN_COMMAND)/',
+		'/(?<status>INSERTED)\s(?<id>\d+)/',
+		'/(?<status>BURIED)\s(?<id>\d+)/',
+		'/(?<status>EXPECTED_CRLF)/',
+		'/(?<status>JOB_TO_BIG)/',
+		'/(?<status>DRAINING)/',
+		'/(?<status>USING)\s(?<tube>.+)/',
+		'/(?<status>DEADLINE_SOON)/',
+		'/(?<status>TIMED_OUT)/',
+		'/(?<status>RESERVED)\s(?<id>\d+)\s(?<bytes>\d+)\r\n(?<data>.*)/',
+		'/(?<status>DELETED)/',
+		'/(?<status>NOT_FOUND)/',
+		'/(?<status>RELEASED)/',
+		'/(?<status>TOUCHED)/',
+		'/(?<status>WATCHING)\s(?<count>\d+)/',
+		'/(?<status>NOT_IGNORED)/',
+		'/(?<status>KICKED)\s(?<count>\d+)/',
+		'/(?<status>KICKED)/',
+		'/(?<status>OK)\s(?<bytes>\d+)\r\n(?<data>.*)/s',
+	);
+
+	public function __construct(array $config = array()) {
+		parent::__construct($config);
+
+		if($this->_config['message']) {
+			$this->data = $this->_parseResponse($this->_config['message']);
+		}
+	}
+
+	protected function _parseResponse($message) {
+		foreach ($this->_responseTypes as $pattern) {
+			if(preg_match($pattern, $message, $match)) {
+				$this->id = (isset($match['id'])) ? $match['id'] : null ;
+				$this->status = (isset($match['status'])) ? $match['status'] : null ;
+				$this->tube = (isset($match['tube'])) ? $match['tube'] : null ;
+				$this->bytes = (isset($match['bytes'])) ? $match['bytes'] : null ;
+				$this->count = (isset($match['count'])) ? $match['count'] : null ;
+
+				if($this->status == "OK" && $this->bytes > 0) {
+					$match['data'] = $this->_parseYaml($match['data']);
+				}
+
+				return (isset($match['data'])) ? $match['data'] : null ;
+			}
+		}
+		return null;
+	}
+
+	protected function _parseYaml($data) {
+		if(extension_loaded('yaml')) {
+			return yaml_parse($data);
+		}
+		return $data;
+	}
+
+}
+
+?>
